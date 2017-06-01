5d3038302ff9e4b78aac891a229e07efe8a67f6b
Mateusz Popielarz <rogoznocraft@gmail.com>
2017-06-01 13:47:03 +0200

    Moved files

---
+++ src/WebCrawler/WebCrawler/Model/Benchmarking/EntryWithDescendantsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Model.Benchmarking
+{
+    public class EntryWithDescendantsViewModel :EntryViewModel
+    {
+        public EntryWithDescendantsViewModel(string uri, int miliSeconds, IEnumerable<string> descendants) : base(uri, miliSeconds)
+        {
+            this.Descentants = descendants;
+        }
+
+        public IEnumerable<string> Descentants { get; }
+    }
+}
