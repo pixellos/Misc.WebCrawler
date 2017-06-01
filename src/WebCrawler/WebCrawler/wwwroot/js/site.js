7f4d241d9302f2c3df9fd6f0bd8797b7f2aeee70
Mateusz Popielarz <rogoznocraft@gmail.com>
2017-06-01 09:56:52 +0200

    Fixed input bug

--- src/WebCrawler/WebCrawler/wwwroot/js/site.js
+++ src/WebCrawler/WebCrawler/wwwroot/js/site.js
@@ -1,38 +1,56 @@
-﻿function initializeAjax(targetSelector, activatorSelector, inputSelector, ajaxUrl) {
+﻿"use strict";
+
+function initializeAjax(targetSelector, activatorSelector, inputSelector, ajaxUrl, successCallback) {
     let circularFlashingColors =
         `<div class="progress">
-    <div class="indeterminate"></div>
+<div class="indeterminate"></div>
 </div>`;
+
     function changeDataIn(selector, data) {
         $(selector).empty();
         $(selector).append(data);
     }
-    function onSucceess(data, textStatus, jqXHR) {
+
+    function onSuccess(data, textStatus, jqXHR) {
         if (textStatus != 'success') {
             Materialize.toast(`Sorry, something went bad, please try again. If error will repeat contact an administrator. Cause: ${textStatus}`, 900);
             console.error(textStatus);
         }
-        changeDataIn(targetSelector, data);
+        var dataToChange = data;
+        if (typeof successCallback === 'function') {
+            dataToChange = successCallback(targetSelector, data);
+        }
+        else {
+            changeDataIn(targetSelector, dataToChange);
+        }
     };
+
     function onError() {
         changeDataIn(targetSelector, "");
         Materialize.toast("Sorry, your input is invalid. Please provide valid http://adresss.com .", 1000);
     }
+
     function fixInput(selector) {
         let inputData = $(selector).val();
         if (!inputData.startsWith("http://") && !inputData.startsWith("https://")) {
             Materialize.toast("Let me fix this URI for you :)", 1600);
             var fixed = "http://" + inputData;
             $(selector).val(fixed)
+            $(selector).html(fixed)
             return fixed;
         }
+        else {
+            return inputData;
+        }
     }
+
     function downloadData() {
         changeDataIn(targetSelector, circularFlashingColors);
         var requestedUrl = fixInput(inputSelector);
+
         $.ajax({
             url: ajaxUrl,
-            success: onSucceess,
+            success: onSuccess,
             error: onError,
             data: {
                 url: requestedUrl
@@ -54,16 +72,57 @@ $(document).ready(function () {
             }
         });
     })();
+
     (function historyButtonInit() {
         let targetSelector = "#bottomData";
         let activatorSelector = "#historyButton";
         let ajaxUrl = "/WebStatistics/SiteHistory";
         initializeAjax(targetSelector, activatorSelector, input, ajaxUrl)
     })();
-    (function minMax() {
+
+    (function realtimeButtonInit() {
+        let targetSelector = "#topData";
+        let activatorSelector = "#realtimeButton";
+        let ajaxUrl = "/WebStatistics/SingleSiteRaw";
+        function createChart(selector) {
+            $(selector).html("<canvas id=\"graph\"> </canvas>");
+            let ctx = document.getElementById('graph').getContext('2d');
+            let chart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    datasets: [{
+                        label: 'miliseconds',
+                    }]
+                }
+            });
+            return chart;
+        }
+        function onSuccess(selector, data) {
+            let target = $(targetSelector);
+            var chart;
+            if (target.attr("chart") != 'is') {
+                target.empty();
+                chart = createChart(targetSelector);
+                console.log(data);
+                addData(chart, data.uri, data.descendants);
+            }
+            else {
+                addData(chart, data.Uri, data.Descendants);
+            }
+        }
+        initializeAjax(targetSelector, activatorSelector, input, ajaxUrl, onSuccess)
+    })();
+
+    (function minMaxButtonInit() {
         let targetSelector = "#bottomData";
         let activatorSelector = "#minMaxButton";
         let ajaxUrl = "/WebStatistics/MinMaxValues";
         initializeAjax(targetSelector, activatorSelector, input, ajaxUrl)
     })();
 });
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets[0].data.push(data);
+    chart.update();
+}
